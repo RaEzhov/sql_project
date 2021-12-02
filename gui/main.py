1e8024041e9e361dd29144046e3b801295ad0ace
RaEzhov <ezhov.mail@yandex.ru>
2021-12-02 22:53:32 +0300

    excel file added, scheme added

--- gui/main.py
+++ gui/main.py
@@ -1,4 +1,5 @@
-from tkinter import *
+import tkinter as tk
+import tkinter.messagebox as mb
 
 default_width = 1280
 default_height = 720
@@ -6,40 +7,50 @@ default_font = 'Terminal'
 default_resolution = f'{default_width}x{default_height}'
 
 
-root = Tk()
-root.geometry(default_resolution+'+120+50')
-root.title("Database editor")
-root.resizable(width=False, height=False)
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        #background image
+        self.filename = tk.PhotoImage(file='./bg_im.gif')
+        self.background_label = tk.Label(self, image=self.filename)
+        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        self.button_background = tk.Canvas(self, width=400, height=720, bg="#90c", highlightthickness=0)
+
+        self.b1 = tk.Button(text='Button', width=30, height=3, font=default_font+' 20', background='#cf0', foreground='#639', activebackground='#9c0', activeforeground='#639', bd=5)
+        self.b2 = tk.Button(text='Button', width=30, height=3, font=default_font+' 20', background='#cf0', foreground='#639', activebackground='#9c0', activeforeground='#639', bd=5)
+        self.b3 = tk.Button(text='Button', width=30, height=3, font=default_font+' 20', background='#cf0', foreground='#639', activebackground='#9c0', activeforeground='#639', bd=5)
+        self.b4 = tk.Button(text='Button', width=30, height=3, font=default_font+' 20', background='#cf0', foreground='#639', activebackground='#9c0', activeforeground='#639', bd=5)
+        self.b5 = tk.Button(text='Button', width=30, height=3, font=default_font+' 20', background='#cf0', foreground='#639', activebackground='#9c0', activeforeground='#639', bd=5)
+        self.b6 = tk.Button(text='Button', width=12,  height=3, font=default_font+' 20', background='#cf0', foreground='#639', activebackground='#9c0', activeforeground='#639', bd=5)
+        self.b7 = tk.Button(text='Button', width=12,  height=3, font=default_font+' 20', background='#cf0', foreground='#639', activebackground='#9c0', activeforeground='#639', bd=5)
+
+        self.b1.place(x=930, y=20)
+        self.b2.place(x=930, y=120)
+        self.b3.place(x=930, y=220)
+        self.b4.place(x=930, y=320)
+        self.b5.place(x=930, y=420)
+        self.b6.place(x=930, y=520)
+        self.b7.place(x=1110, y=520)
+
+        self.info_output = tk.Label(width=83, height=20, font=default_font+' 20', text='helloooooo\nthis is text', foreground='#222', background='#93c', borderwidth=5,  relief="ridge", anchor='nw')
+        self.info_output.place(x=20, y=100)
+
+        self.button_background.create_line(5, 5, 395, 5, fill='#528', dash=(40, 40), width=5)
+        self.button_background.create_line(395, 5, 395, 715, fill='#528', dash=(40, 40), width=5)
+        self.button_background.create_line(5, 715, 395, 715, fill='#528', dash=(40, 40), width=5)
+        self.button_background.create_line(5, 715, 5, 5, fill='#528', dash=(40, 40), width=5)
+        self.button_background.pack(side=tk.RIGHT)
+
+    def hello(self):
+        msg = 'Hello, world!'
+        mb.showerror('Info', msg)
 
 
-
-#background image
-filename = PhotoImage(file='./bg_im.gif')
-background_label = Label(root, image=filename)
-background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-
-button_background = Canvas(root, width=400, height=720, bg="#90c", highlightthickness=0)
-button_background.pack(side=RIGHT)
-
-
-b1 = Button(text='Button', width=30, height=3, font=default_font+' 20', background='#cf0', foreground='#639', activebackground='#9c0', activeforeground='#639', bd=5)
-b2 = Button(text='Button', width=30, height=3, font=default_font+' 20', background='#cf0', foreground='#639', activebackground='#9c0', activeforeground='#639', bd=5)
-b3 = Button(text='Button', width=30, height=3, font=default_font+' 20', background='#cf0', foreground='#639', activebackground='#9c0', activeforeground='#639', bd=5)
-b4 = Button(text='Button', width=30, height=3, font=default_font+' 20', background='#cf0', foreground='#639', activebackground='#9c0', activeforeground='#639', bd=5)
-b5 = Button(text='Button', width=30, height=3, font=default_font+' 20', background='#cf0', foreground='#639', activebackground='#9c0', activeforeground='#639', bd=5)
-b6 = Button(text='Button', width=12,  height=3, font=default_font+' 20', background='#cf0', foreground='#639', activebackground='#9c0', activeforeground='#639', bd=5)
-b7 = Button(text='Button', width=12,  height=3, font=default_font+' 20', background='#cf0', foreground='#639', activebackground='#9c0', activeforeground='#639', bd=5)
-b1.place(x=930, y=20)
-b2.place(x=930, y=120)
-b3.place(x=930, y=220)
-b4.place(x=930, y=320)
-b5.place(x=930, y=420)
-b6.place(x=930, y=520)
-b7.place(x=1110, y=520)
-
-info_output = Label(width=83, height=20, font=default_font+' 20', text='helloooooo\nthis is text', foreground='#222', background='#93c', borderwidth=5,  relief="ridge", anchor='nw')
-info_output.place(x=20, y=100)
-
-
-root.mainloop()
+if __name__ == '__main__':
+    app = App()
+    app.geometry(default_resolution + '+120+50')
+    app.title("Database editor")
+    app.resizable(width=False, height=False)
+    app.mainloop()
